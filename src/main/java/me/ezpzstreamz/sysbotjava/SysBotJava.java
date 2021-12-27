9de860ddfdf8466a40401e5b42f8580207dfe101
Steve Cook <steviecook210@gmail.com>
2021-12-27 11:54:19 -0500

    Refactoring Added nickname selections

--- src/main/java/me/ezpzstreamz/sysbotjava/SysBotJava.java
+++ src/main/java/me/ezpzstreamz/sysbotjava/SysBotJava.java
@@ -11,6 +11,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 import javax.security.auth.login.LoginException;
 import java.io.*;
@@ -20,8 +21,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class SysBotJava implements Runnable {
 
@@ -32,7 +32,8 @@ public class SysBotJava implements Runnable {
     private final SysBotController sbc;
     private final Map<String, String> pointers;
 
-    private final byte[] pkm;
+    private final Map<String, byte[]> pkmFiles;
+
     private long startTime;
 
     private static final List<Short> tradeSpecies = new ArrayList<>() {{
@@ -60,6 +61,7 @@ public class SysBotJava implements Runnable {
         JDA jdaTemp = null;
         TwitchClient tcTemp = null;
         pointers = new HashMap<>();
+        pkmFiles = new HashMap<>();
         try (InputStream resource = SysBotJava.class.getClassLoader().getResourceAsStream("pointers.txt")) {
             assert resource != null;
             List<String> lines = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8)).lines()
@@ -104,12 +106,25 @@ public class SysBotJava implements Runnable {
                 }
             });
         }
-        pkm = Files.readAllBytes(Paths.get("pkb/pokemon.pb8"));
+        File pokemonFolder = new File("pkb");
+        FileFilter filter = new WildcardFileFilter("*.pb8");
+        File[] pokemon = pokemonFolder.listFiles(filter);
+        assert pokemon != null;
+        for (File f : pokemon) {
+            pkmFiles.put(f.getName().substring(0, f.getName().length() - 4),
+                    Files.readAllBytes(Paths.get(f.getPath())));
+        }
         sbc = new SysBotController("10.0.0.53", 6000);
         sbc.connect();
         userList = new ArrayDeque<>();
         jda = jdaTemp;
         twitchClient = tcTemp;
+        if (twitchClient != null) {
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            scheduler.scheduleAtFixedRate(() -> twitchClient.getChat().sendMessage("EzPzStreamz",
+                            "Welcome to the beta testing of the bot! Type \"!q join\" to join and \"!q pos\" to see your current position in the queue. "),
+                    1, 5, TimeUnit.MINUTES);
+        }
         sbc.sendCommand("configure keySleepTime 35");
     }
 
@@ -135,7 +150,7 @@ public class SysBotJava implements Runnable {
         List<Map.Entry<String, String>> tempList = userList.stream().toList();
         for (int i = 0; i < tempList.size(); i++) {
             if (tempList.get(i).getKey().equalsIgnoreCase(mode) && tempList.get(i).getValue().equalsIgnoreCase(userID))
-                return i;
+                return i + 1;
         }
         return -1;
     }
@@ -150,15 +165,29 @@ public class SysBotJava implements Runnable {
 
     private void hideHider() {
         File hiderImage = new File("hide.png");
-        if(hiderImage.exists()) {
+        if (hiderImage.exists()) {
             hiderImage.renameTo(new File("show.png"));
         }
     }
 
     private void showHider() {
         File hiderImage = new File("show.png");
-        if(hiderImage.exists()) {
+        if (hiderImage.exists()) {
             hiderImage.renameTo(new File("hide.png"));
+        }
+    }
+
+    public void waitNextTradePhase(int nextPhase) throws IOException, InterruptedException {
+        while (Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) < nextPhase) {
+            if (hasTimedOut()) {
+                System.out.println("Trade timed out.");
+                sbc.click("B", 1500);
+                leaveTrade();
+                Thread.sleep(500);
+                leaveUnionRoom();
+                return;
+            }
+            Thread.sleep(500);
         }
     }
 
@@ -198,7 +227,15 @@ public class SysBotJava implements Runnable {
             sbc.click("DDOWN", 1200);
             sbc.click("A", 1200);
 
-            sbc.poke(pointers.get("b1s1"), "0x" + Util.bytesToHex(Util.encryptPb8(pkm)));
+            if (pkmFiles.containsKey("default")) {
+                sbc.poke(pointers.get("b1s1"), "0x" + Util.bytesToHex(Util.encryptPb8(pkmFiles.get("default"))));
+            } else {
+                List<String> names = pkmFiles.keySet().stream().toList();
+                int randomName = ThreadLocalRandom.current().nextInt(0, names.size());
+                sbc.poke(pointers.get("b1s1"),
+                        "0x" + Util.bytesToHex(Util.encryptPb8(pkmFiles.get(names.get(randomName)))));
+            }
+
 
             Thread.sleep(600);
             startTime = TimeUnit.SECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS);
@@ -236,31 +273,42 @@ public class SysBotJava implements Runnable {
                 int currentPhase = 3;
                 int nextPhase = 4;
 
-                sbc.click("A", 1000);
-                sbc.click("A", 1000);
+                sbc.click("A", 1300);
+                sbc.click("A", 1300);
 
-                while (Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) < nextPhase) {
-                    if (checkIfCancelled(currentPhase)) return;
-                    if (hasTimedOut()) {
-                        System.out.println("Trade timed out.");
-                        sbc.click("B", 1500);
-                        leaveTrade();
-                        Thread.sleep(500);
-                        //leave union room
-                        leaveUnionRoom();
-                        return;
-                    }
-                    Thread.sleep(500);
-                }
+                waitNextTradePhase(nextPhase);
 
                 startTime += 15;
 
-                if(checkIfCancelled(currentPhase)) return;
+                if (checkIfCancelled(currentPhase)) return;
                 currentPhase = nextPhase;
                 nextPhase = 6;
                 Thread.sleep(500);
                 String pb8Str = sbc.peek(pointers.get("tradePB8"), 0x158);
                 byte[] pb8 = Util.decryptEb8(HexFormat.of().parseHex(pb8Str));
+                byte[] nickname = Arrays.copyOfRange(pb8, 88, 112);
+                String nicknameStr =
+                        new String(nickname, StandardCharsets.ISO_8859_1).trim().replaceAll("\0", "").toLowerCase(
+                                Locale.ROOT);
+                if (pkmFiles.containsKey(nicknameStr)) {
+                    sbc.click("B", 1600);
+                    sbc.poke(pointers.get("b1s1"), "0x" + Util.bytesToHex(Util.encryptPb8(pkmFiles.get(nicknameStr))));
+                    sbc.click("A", 1300);
+                    sbc.click("A", 1300);
+
+                    currentPhase = 3;
+                    nextPhase = 4;
+
+                    waitNextTradePhase(nextPhase);
+
+                    if (checkIfCancelled(currentPhase)) return;
+
+                    currentPhase = nextPhase;
+                    nextPhase = 6;
+
+                }
+                pb8Str = sbc.peek(pointers.get("tradePB8"), 0x158);
+                pb8 = Util.decryptEb8(HexFormat.of().parseHex(pb8Str));
                 ByteBuffer bb = ByteBuffer.wrap(pb8, 8, 10);
                 bb.order(ByteOrder.LITTLE_ENDIAN);
                 short species = bb.getShort();
@@ -268,6 +316,7 @@ public class SysBotJava implements Runnable {
                 bb = ByteBuffer.wrap(pb8, 140, 144);
                 bb.order(ByteOrder.LITTLE_ENDIAN);
                 boolean isEgg = (bb.getInt() >> 30 & 0x1) == 1;
+
                 if (isEgg || tradeSpecies.contains(species)) {
                     System.out.println("Egg or trade species offered.");
                     sbc.click("B", 1500);
@@ -277,30 +326,19 @@ public class SysBotJava implements Runnable {
                     leaveUnionRoom();
                     return;
                 }
-                sbc.click("A", 1000);
 
-                while(Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) < nextPhase) {
-                    if (checkIfCancelled(currentPhase)) return;
-                    if (hasTimedOut()) {
-                        System.out.println("Trade timed out.");
-                        sbc.click("B", 1500);
-                        leaveTrade();
-                        Thread.sleep(500);
-                        leaveUnionRoom();
-                        return;
-                    }
-                    Thread.sleep(500);
-                }
-                if(checkIfCancelled(currentPhase)) return;
+                sbc.click("A", 1300);
+
+                waitNextTradePhase(nextPhase);
+                if (checkIfCancelled(currentPhase)) return;
                 currentPhase = nextPhase;
                 nextPhase = 9;
 
-
-
-                while(Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) < nextPhase) {
-                    if (checkIfCancelled(currentPhase)) return;
+                while (Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) < nextPhase) {
                     sbc.click("B", 1500);
                 }
+
+                if (checkIfCancelled(currentPhase)) return;
 
                 while (Integer.parseInt(sbc.peek(pointers.get("tradeFlowState"), 1), 16) == 3) {
                     Thread.sleep(500);
@@ -317,7 +355,7 @@ public class SysBotJava implements Runnable {
     }
 
     private boolean checkIfCancelled(int currentPhase) throws InterruptedException, IOException {
-        if(Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) < currentPhase) {
+        if (Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) < currentPhase) {
             System.out.println("Trade cancelled.");
             Thread.sleep(1000);
             sbc.click("B", 1500);
@@ -326,7 +364,7 @@ public class SysBotJava implements Runnable {
             leaveUnionRoom();
             return true;
         }
-        if(Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) == 0xA) {
+        if (Integer.parseInt(sbc.peek(pointers.get("netTradePhase"), 1), 16) == 0xA) {
             System.out.println("Trade cancelled.");
             Thread.sleep(1000);
             sbc.click("B", 1500);
